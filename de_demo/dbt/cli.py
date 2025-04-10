2a721be3e0205aa2419edbaa2d21ac1178af6cf0
Александр Петров <petrov.a.o-86@yandex.ru>
2025-04-10 10:52:04 +0500

    fix dbt cli

--- de_demo/dbt/cli.py
+++ de_demo/dbt/cli.py
@@ -3,10 +3,9 @@ from dbt.cli.main import dbtRunner
 
 class DbtCli:
     """Запуск dbt."""
-    def __call__(self, *args, log_path: str = "dbt/logs", target_path: str = "dbt/target", **kwargs):
+    def __call__(self, *args, project_dir: str = "dbt", **kwargs):
         dbt = dbtRunner()
-        kwargs["log_path"] = log_path
-        kwargs["target_path"] = target_path
+        kwargs["project_dir"] = project_dir
         result = dbt.invoke(list(args), **kwargs)
 
         if not result.success:
